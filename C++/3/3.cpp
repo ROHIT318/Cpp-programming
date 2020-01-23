d76e140418d1209a0b4c6b563b8b112d03b62f6e
ROHIT SHARMA <38538605+ROHIT318@users.noreply.github.com>
2020-01-23 12:22:21 +0530

    Added question and comment to 3.cpp

--- C++/3/3.cpp
+++ C++/3/3.cpp
@@ -1,55 +1,68 @@
+/*  Design a class BitOp containing two integer data members. The class should contain methods like BitAnd,
+	BitOr and BitXor to do bitwise AND, bitwise OR, and bitwise XOR operations respectively on the data
+	members of the objects. Write suitable constructors for inializing the objects. Write a display funcons to
+	show the results in binary form.
+*/
+
 #include <iostream>
 using namespace std; 
 
 class BitOp
 {
     private:
+		// Declaring data members as private
         int var1; 
         int var2; 
         int result; 
     
     public:
+		// constructor declaration
         BitOp()
         {
             var1 = 0; 
             var2 = 0; 
         }
+		// constructor overloading
         BitOp(int number1, int number2)
         {
             var1 = number1; 
             var2 = number2; 
         } 
 
+		// all member function declaration
         void BitAnd(); 
         void BitOr(); 
         void BitXor(); 
         void decToBinary(); 
-        int returnBinary(int number); 
-        int reverse(int number); 
+        // int returnBinary(int number); 
+        // int reverse(int number); 
 }; 
 
+// performing bitwise AND operation
 void BitOp::BitAnd()
 {
     result = var1 & var2; 
 }
 
+// performing bitwise OR operation
 void BitOp::BitOr()
 {
     result = var1 | var2; 
 }
 
+// performing bitwise XOR operation
 void BitOp::BitXor()
 {
     result = var1 ^ var2; 
 }
 
 
-
+// converting decimal to binary number
 void BitOp::decToBinary() 
 { 
 	int counter = 0, number; 
     int binaryNum[32]; 
-    
+    // Due to 4 bit integer counter counting from 0 to 3
     while(counter < 3)
     { 
         if(counter == 0)
@@ -68,13 +81,14 @@ void BitOp::decToBinary()
             cout << "Result is "; 
         }
         int i = 0; 
+		// storing the remainders in an array after dividing by 2
         while (number > 0)
         { 
             binaryNum[i] = number % 2; 
             number = number / 2; 
             i++; 
         } 
-   
+   		// displaying the array in rverse manner
         for (int j = i - 1; j >= 0; j--) 
         {
             cout << binaryNum[j]; 
