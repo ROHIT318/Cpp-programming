54a7b132e6d1fd147e0365225bad5e4bf7606982
ROHIT SHARMA <38538605+ROHIT318@users.noreply.github.com>
2020-01-24 17:11:22 +0530

    Added question and comments in 7.cpp

--- C++/7/7.cpp
+++ C++/7/7.cpp
@@ -1,14 +1,22 @@
+/* Design a class Stack using template class. Write proper functions for performing PUSH, POP operaons on
+    generic data types.
+*/
+
 #include <iostream>
+// size of the stack increase the value for storing more values
 #define size 10
 int top = -1; 
 
 using namespace std; 
 
+// declaring class stack of generic data type
 template<class T> 
 class stack {
-    public: 
+    public:
+        // declaration of data member
         int stack[size]; 
 
+        // function to push element in stack
         void push(T element) {
             if(!isfull()) {
                 top++; 
@@ -19,6 +27,7 @@ class stack {
             }
         }
         
+        // function to pop element in stack
         T pop() {
             if(isempty()) {
                 cout << "\nStack underflow.\n"; 
@@ -30,12 +39,14 @@ class stack {
             }
         }
 
+        // function to check if stack is empty
         bool isempty() {
             if(top == 0) 
                 return 1; 
             return 0; 
         }
         
+        // function to check if stack is full
         bool isfull() {
             if(top == size) 
                 return 1; 
@@ -43,6 +54,7 @@ class stack {
         }
 }; 
 
+// need to modify main function using infinite while and switch cases
 int main() {
     stack<int> a; 
 
